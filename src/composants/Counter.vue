8c6b5162cc369ff3bb971505d6e245fa9b686272
antoine <antoine.godeau@sfr.fr>
2019-05-28 00:17:46 +0200

    Initilisation du projet en CLI

---
+++ src/composants/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="counter">
+    {{ count }}
+    <div class="button" @click="increments">clique-ici</div>
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+  data (){
+    return {
+      count: 0
+    }
+  },
+    methods :{
+      increments (){
+        this.count++
+      }
+    }
+  }
+</script>
+
+<style src="../styles/Counter.css"></style>
